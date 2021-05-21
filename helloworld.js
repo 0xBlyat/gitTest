2694c50bccf76021bd18440f2411de52a3c467ff
Blyat <tony.alhwayek@gmail.com>
2021-05-21 02:10:26 -0700

    Added annoying loop for alert

--- helloworld.js
+++ helloworld.js
@@ -2,7 +2,10 @@ console.log("Hello world")
 
 alert("WORLD DOES NOT SAY HELLO")
 
+var i;
+for (i = 0; i < 10; i++) {
 alert("YOU WON OUR 100000th VISITOR GIVEAWAY!!")
+}
 
 alert("ok you didn't win anything")
 
