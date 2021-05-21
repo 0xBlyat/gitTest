fe047096a685e036b2c2938c1fbe423ec551ce4c
Blyat <tony.alhwayek@gmail.com>
2021-05-21 02:26:46 -0700

    Added a button function

--- helloworld.js
+++ helloworld.js
@@ -1,12 +1,14 @@
-console.log("Hello world")
-
 alert("WORLD DOES NOT SAY HELLO")
 
-var i;
-for (i = 0; i < 10; i++) {
-alert("YOU WON OUR 100000th VISITOR GIVEAWAY!!")
-}
+// var i;
+// for (i = 0; i < Math.random() * (5 - 0) + 0; i++) {
+// alert("YOU WON OUR 100000th VISITOR GIVEAWAY!!")
+// }
 
 alert("ok you didn't win anything")
 
 alert("potato")
+
+document.getElementById("button").onclick = function() {
+alert("HELLO BACK")
+}
